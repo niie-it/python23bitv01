1b8b3d55357005afc25c4ede230e82f06fafc34f
HIENLTH 2023 <hyhien@gmail.com>
2024-10-18 16:16:42 +0700

    Demo Python Basic

---
+++ Buoi01/Demo02.py
@@ -0,0 +1,14 @@
+# Demo minh họa lý thuyết python02
+
+x = 1
+print(x, type(x))
+x = True
+print(x, type(x))
+x = 8.9
+print(x, type(x))
+x = "Tèo"
+print(x, type(x))
+x = [1,3]
+print(x, type(x))
+x = {"id": 5, "value": 8.9}
+print(x, type(x))
--- Buoi01/README.md
+++ Buoi01/README.md
@@ -3,4 +3,7 @@
 - Nhập / xuất
 - Chuyển đổi kiểu dữ liệu: int(), str(), float(), bool()
 - Xử lý chuỗi:
+
   - Bỏ khoảng trắng 2 bên: .strip()
+
+- Slide: đã xong Slide Python01,Python02
