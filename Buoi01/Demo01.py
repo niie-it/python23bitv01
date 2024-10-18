5224803f10d52fd7d0a1cf6273484c995193883e
HIENLTH 2023 <hyhien@gmail.com>
2024-10-18 15:14:09 +0700

    First demo

---
+++ Buoi01/Demo01.py
@@ -0,0 +1,9 @@
+# Đây là ghi chú 1 dòng
+'''Ghi chú nhiều dòng
+Dùng cặp 3 dấu nháy đơn hay nháy kép'''
+print('PYTHON 23BITV01')
+print()
+print("Xin chào các bạn")
+
+ten_ban = input("Tên của bạn: ")
+print("Chào bạn", ten_ban)
